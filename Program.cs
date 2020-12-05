26f790469db77d74b441645851ce50fa84444683
Arjan ten Hoeve <arjan.ten.hoeve@achmea.nl>
2020-12-05 23:21:09 +0100

    deel 2 opgelost.

--- Program.cs
+++ Program.cs
@@ -15,20 +15,32 @@ namespace aoc3
             //lengte regel: 11  (example)
             int linelength = 31;
             var row = new char[linelength];
-            int myposition = 1;
+            int myposition = 1;   //positie begint gewoon bij 1. zoals in de som
             int treecount = 0;
+            int right = 1;  //aantal stappen naar rechts   bij deel1 right = 3
+            int down = 2;    //aantal stappen naar beneden. bij deel1 down=1     
+            int downcount = 1;  //houdt aantal stappen bij dat down is gezet. de eerste regel moet altijd geteld worden daarom initialisatie op 1
             foreach (string line in File.ReadLines(@"c:\repos\aoc3\input\terrain.txt"))
             {
                 row = line.ToCharArray();
+                downcount++;
+                if (downcount < down) { continue; } else { downcount = 0; }  //zorg dat regels worden overgeslagen bij down groter dan 1
+
                 Console.WriteLine($"{line} position: {myposition} character: {row[myposition-1]}");
                 if (row[myposition - 1] == '#') treecount++;  //als je op boom staat dan deze tellen
+
                 
-                myposition += 3;
-                //myposition %= linelength;  //niet zeker.
+                myposition += right;
                 if (myposition > linelength) { myposition -= linelength; }  // als je over de 31 heen gaan, dan vooraan beginnen.
+
                 
+
             }
-            Console.WriteLine($"aantal bomen: {treecount}");  //goede antwoord: 257
+            Console.WriteLine($"aantal bomen: {treecount}");  //goede antwoord: op deel 1= 257
+
+            // goede antwoord op deel 2: 61 * 257 * 64 * 47 * 37 = 1744787392
+
+
 
         }
     }
