372ddf78eeb9339b53eed9685758da881de57dfe
Arjan ten Hoeve <arjan.ten.hoeve@achmea.nl>
2020-12-05 22:14:16 +0100

    kleine wijzigingen

--- Program.cs
+++ Program.cs
@@ -4,28 +4,31 @@ using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-namespace aoc2
+namespace aoc3
 {
     class Program
     {
         static void Main(string[] args)
         {
 
-            //lengte regel: 31
-            //lengte regel: 11
-            int linelenght = 31;
-            var row = new char[linelenght];
-            int myposition = 0;
+            //lengte regel: 31  (terrain)
+            //lengte regel: 11  (example)
+            int linelength = 31;
+            var row = new char[linelength];
+            int myposition = 1;
             int treecount = 0;
-            foreach (string line in File.ReadLines(@"c:\repos\aoc3\input\example.txt"))
+            foreach (string line in File.ReadLines(@"c:\repos\aoc3\input\terrain.txt"))
             {
                 row = line.ToCharArray();
-                Console.WriteLine($"{line} position: {position} character: {row[myposition]}");
+                Console.WriteLine($"{line} position: {myposition} character: {row[myposition-1]}");
+                if (row[myposition - 1] == '#') treecount++;  //als je op boom staat dan deze tellen
+                
                 myposition += 3;
-                myposition %= (linelenght-1);  //als i > 30, begin dan vooraan door er 30 af te  halen (ofwel rest van deling door 30)
-                if (row[myposition] == '#') treecount++;  //als je op boom staat dan deze tellen
+                //myposition %= linelength;  //niet zeker.
+                if (myposition > linelength) { myposition -= linelength; }  // als je over de 31 heen gaan, dan vooraan beginnen.
+                
             }
-            Console.WriteLine($"aantal bomen: {treecount}");
+            Console.WriteLine($"aantal bomen: {treecount}");  //goede antwoord: 257
 
         }
     }
